48850dff49b7424d4ef53bf8844029a768961892
y-du <42994541+y-du@users.noreply.github.com>
2018-10-25 23:31:00 +0200

    store objects in __sections instead of classes replaced for loop with one-liner

--- easy_conf/__init__.py
+++ easy_conf/__init__.py
@@ -29,19 +29,16 @@ class Configuration:
         return cls._instance
 
     def __init__(self, conf_file, user_path=None, exit_after_create=True):
-        self.__sections = [item for item in self.__class__.__dict__.values() if inspect.isclass(item) and issubclass(item, Section)]
-
-        for section in self.__sections:
-            self.__setattr__(section.__name__, section(self.__setKey))
-
+        self.__sections = {item.__name__: item((self.__setKey)) for item in self.__class__.__dict__.values() if inspect.isclass(item) and issubclass(item, Section)}
+        self.__dict__ = {**self.__dict__ , **self.__sections}
         self.__conf_path = user_path if user_path else os.path.abspath(os.path.split(inspect.getfile(inspect.stack()[-1].frame))[0])
         self.__conf_file = conf_file
         self.__parser = configparser.ConfigParser()
 
         if not os.path.isfile(os.path.join(self.__conf_path, self.__conf_file)):
             print("Config file '{}' not found".format(self.__conf_file))
-            for section in self.__sections:
-                self.__parser[section.__name__] = self.__classToDict(section)
+            for section in self.__sections.values():
+                self.__parser[section.__class__.__name__] = self.__classToDict(section.__class__)
             self.__writeConfFile()
             print("Created config file '{}' at '{}'".format(self.__conf_file, self.__conf_path))
             if exit_after_create:
@@ -75,9 +72,7 @@ class Section:
     def __setattr__(self, key, value):
         if key in self.__dict__.keys():
             super().__setattr__(key, value)
-            if not 'parent_obj' in key:
-                print('{}.{}->{}'.format(self, key, value))
-                self.setCallbk(self.__class__.__name__, key, value)
+            self.setCallbk(self.__class__.__name__, key, value)
         else:
             err_msg = "assignment of new attribute '{}' to '{}' not allowed".format(key, self.__class__.__qualname__)
             raise AttributeError(err_msg)
