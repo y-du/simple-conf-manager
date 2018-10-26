b31c3cf971976f4e5c56d2e16b8c311df5f050cd
y-du <42994541+y-du@users.noreply.github.com>
2018-10-26 11:44:00 +0200

    work in progress...

--- easy_conf/__init__.py
+++ easy_conf/__init__.py
@@ -14,7 +14,7 @@
    limitations under the License.
 """
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 
 import os, inspect, configparser
@@ -29,26 +29,55 @@ class Configuration:
         return cls._instance
 
     def __init__(self, conf_file, user_path=None, exit_after_create=True):
-        self.__sections = {item.__name__: item((self.__setKey)) for item in self.__class__.__dict__.values() if inspect.isclass(item) and issubclass(item, Section)}
-        self.__dict__ = {**self.__dict__ , **self.__sections}
+        self.__section_classes = {item.__name__: item for item in self.__class__.__dict__.values() if inspect.isclass(item) and issubclass(item, Section)}
+        #self.__dict__ = {**self.__dict__ , **self.__sections}
         self.__conf_path = user_path if user_path else os.path.abspath(os.path.split(inspect.getfile(inspect.stack()[-1].frame))[0])
         self.__conf_file = conf_file
         self.__parser = configparser.ConfigParser()
 
         if not os.path.isfile(os.path.join(self.__conf_path, self.__conf_file)):
             print("Config file '{}' not found".format(self.__conf_file))
-            for section in self.__sections.values():
-                self.__parser[section.__class__.__name__] = self.__classToDict(section.__class__)
+            for section in self.__section_classes.values():
+                self.__parser[section.__name__] = self.__sectionClassToDict(section)
             self.__writeConfFile()
             print("Created config file '{}' at '{}'".format(self.__conf_file, self.__conf_path))
             if exit_after_create:
                 exit()
-        else:
-            self.__parser.read(os.path.join(self.__conf_path, self.__conf_file))
 
-    def __classToDict(self, cls):
+        if not self.__parser.sections():
+            try:
+                print("Opening config file '{}' at '{}'".format(self.__conf_file, self.__conf_path))
+                self.__parser.read(os.path.join(self.__conf_path, self.__conf_file))
+                self.__loadConfig()
+            except Exception as ex:
+                print(ex)
+
+    def __loadConfig(self):
+        missing_sections, unknown_sections = self.__diff(self.__section_classes, self.__parser.sections())
+        print("Checking sections ...")
+        for key in unknown_sections:
+            print("Ignoring unknown section '{}'".format(key))
+            #self.__parser.remove_section(key)
+        for key in missing_sections:
+            print("Adding new section '{}'".format(key))
+            self.__parser[key] = self.__sectionClassToDict(self.__section_classes[key])
+            #self.__dict__[key] = self.__section_classes[key](self.__setKey)
+        print("Checking keys ...")
+        for key, section in self.__section_classes.items():
+            #missing_keys, unknown_keys = self.__diff(self.__section_classes[key])
+            print(self.__section_classes[key].__dict__)
+            print(dict(self.__parser[key].items()))
+        self.__writeConfFile()
+
+    def __sectionClassToDict(self, cls):
         return {i: cls.__dict__[i] if type(cls.__dict__[i]) == str else str(cls.__dict__[i]) if cls.__dict__[i] else '' for i in cls.__dict__.keys() if not i.startswith('_')}
 
+    def __diff(self, known, unknown):
+        known_set = set(known)
+        unknown_set = set(unknown)
+        missing = known_set - unknown_set
+        new = unknown_set - known_set
+        return missing, new
 
     def __writeConfFile(self):
         try:
@@ -72,7 +101,7 @@ class Section:
     def __setattr__(self, key, value):
         if key in self.__dict__.keys():
             super().__setattr__(key, value)
-            self.setCallbk(self.__class__.__name__, key, value)
+            self.setCallbk(str(self.__class__.__name__), key, value)
         else:
             err_msg = "assignment of new attribute '{}' to '{}' not allowed".format(key, self.__class__.__qualname__)
             raise AttributeError(err_msg)
